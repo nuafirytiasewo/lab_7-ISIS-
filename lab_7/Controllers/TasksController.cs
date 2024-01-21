f21320abcba1e83f6c755eeb3aee5974fd87d378
детров <devontelnel@mail.ru>
2024-01-21 14:14:53 +0400

    2.1 задание

---
+++ lab_7/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+﻿using lab_7.Models;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_7.Controllers
+{
+    public class TasksController : Controller
+    {
+        private ADOModelDB db = new ADOModelDB();
+        //2.1
+        public ActionResult Index2_1(SearchModelForTask2_1 searchModel)
+        {
+            var data = from r in db.Real_estate_objects
+                       join d in db.Districts on r.District equals d.District_id
+                       where (string.IsNullOrEmpty(searchModel.DistrictName) || d.District_name.Contains(searchModel.DistrictName))
+                       && (!searchModel.MinCost.HasValue || r.Cost >= searchModel.MinCost)
+                       && (!searchModel.MaxCost.HasValue || r.Cost <= searchModel.MaxCost)
+                       orderby r.Cost descending
+                       select new ModelForTask2_1
+                       {
+                           Address = r.Address,
+                           Area = r.Area,
+                           Floor = r.Floor
+                       };
+
+            return View(data.ToList());
+        }
+    }
+}
---
+++ lab_7/Models/ModelForTask2_1.cs
@@ -0,0 +1,9 @@
+﻿namespace lab_7.Models
+{
+    public class ModelForTask2_1
+    {
+        public string Address { get; set; }
+        public decimal? Area { get; set; }
+        public int? Floor { get; set; }
+    }
+}
