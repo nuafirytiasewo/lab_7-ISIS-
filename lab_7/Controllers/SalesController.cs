9505446d385d13652ae68bb61ab6ed9412fba005
детров <devontelnel@mail.ru>
2024-01-21 13:32:05 +0400

    Добавление CRUD к таблицам и отображение на главном экране

---
+++ lab_7/Controllers/SalesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lab_7.Models;
+
+namespace lab_7.Controllers
+{
+    public class SalesController : Controller
+    {
+        private ADOModelDB db = new ADOModelDB();
+
+        // GET: Sales
+        public ActionResult Index()
+        {
+            var sale = db.Sale.Include(s => s.Real_estate_objects).Include(s => s.Realtor);
+            return View(sale.ToList());
+        }
+
+        // GET: Sales/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sale.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Create
+        public ActionResult Create()
+        {
+            ViewBag.Object_id = new SelectList(db.Real_estate_objects, "Object_id", "Address");
+            ViewBag.Realtor_id = new SelectList(db.Realtor, "Realtor_id", "Last_name");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Sale_id,Object_id,Sale_date,Realtor_id,Cost")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sale.Add(sale);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Object_id = new SelectList(db.Real_estate_objects, "Object_id", "Address", sale.Object_id);
+            ViewBag.Realtor_id = new SelectList(db.Realtor, "Realtor_id", "Last_name", sale.Realtor_id);
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sale.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Object_id = new SelectList(db.Real_estate_objects, "Object_id", "Address", sale.Object_id);
+            ViewBag.Realtor_id = new SelectList(db.Realtor, "Realtor_id", "Last_name", sale.Realtor_id);
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Sale_id,Object_id,Sale_date,Realtor_id,Cost")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Object_id = new SelectList(db.Real_estate_objects, "Object_id", "Address", sale.Object_id);
+            ViewBag.Realtor_id = new SelectList(db.Realtor, "Realtor_id", "Last_name", sale.Realtor_id);
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sale sale = db.Sale.Find(id);
+            if (sale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sale sale = db.Sale.Find(id);
+            db.Sale.Remove(sale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
